d26b3c0e9d2b9640d92e4af7b61114a5b16455bc
Corwien <corwien@126.com>
2017-04-11 00:43:04 +0800

    Initial commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->string('password');
+            $table->string('avatar');
+            $table->string('confirmation_token');
+            $table->smallInteger('is_active')->default(0);  // 是否激活了邮箱
+            $table->integer('articles_count')->default(0);
+            $table->integer('likes_count')->default(0);
+            $table->integer('followers_count')->default(0);
+            $table->integer('followings_count')->default(0);
+            $table->string('api_token', 64)->unique();
+            $table->json('settings')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}

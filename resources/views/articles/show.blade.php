8147ed93a1f86491394ffdad5c0100d39e8efe16
Corwien <corwien@126.com>
2017-04-24 01:14:01 +0800

    Add markdown.scss style

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,15 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-        <div class="row">
+        <div class="blog-pages">
             <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{ $article->title }}
+                <div class="panel article-body content-body">
+                    <div class="panel-body">
+                        <h1 class="text-center">
+                            {{ $article->title }}
+                        </h1>
 
-                    </div>
+                        <div class="article-meta text-center">
+                            <i class="fa fa-clock-o"></i> <abbr title="{{ $article->created_at }}" class="timeago">{{ $article->created_at }}</abbr>
+                            ⋅
+                            <i class="fa fa-eye"></i> {{ $article->views_count }}
+                            ⋅
+                            <i class="fa fa-thumbs-o-up"></i> {{ $article->votes_count }}
+                            ⋅
+                            <i class="fa fa-comments-o"></i> {{ $article->comments_count }}
 
-                    <div class="panel-body content">
-                        {!! $article->content !!}
+                        </div>
+
+                        <div class="entry-content">
+                            <div class="markdown" id="emojify">
+                                {!! $article->content !!}
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>

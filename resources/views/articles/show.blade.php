ee78342970343c9e3c79d4b8059966892c2b2ad9
Corwien <corwien@126.com>
2017-05-05 07:46:34 +0800

    Add markdown side menu

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -30,9 +30,31 @@
                     </div>
                 </div>
             </div>
-
+            <div class="col-md-3">
+                <div style="padding:2px;background:#ffffff;margin-top:150px;">
+                    <div id="category"><b>文章目录</b><br/></div>
+                </div>
+            </div>
+            @section('js')
+                <script>hljs.initHighlightingOnLoad();</script>
+                <script type="text/javascript">
+                    $(document).ready(function(){
+                        $("h2,h3,h4,h5,h6").each(function(i,item){
+                            var tag = $(item).get(0).localName;
+                            $(item).attr("id","wow"+i);
+                            $("#category").append('<a class="new'+tag+'" href="#wow'+i+'">'+$(this).text()+'</a></br>');
+                            $(".newh2").css("margin-left",0);
+                            $(".newh3").css("margin-left",20);
+                            $(".newh4").css("margin-left",40);
+                            $(".newh5").css("margin-left",60);
+                            $(".newh6").css("margin-left",80);
+                        });
+                    });
+                </script>
+            @endsection
         </div>
     </div>
+
 @endsection
 
 

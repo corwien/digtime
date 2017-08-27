1ddf4bb82422bae0eb555ab6377df42591c79114
Corwien <corwien@126.com>
2017-08-27 23:31:38 +0800

    add wechat log

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-// use Log;
+use Log;
 
 class WechatController extends Controller
 {
@@ -15,14 +15,15 @@ class WechatController extends Controller
      */
     public function serve()
     {
-        // Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
         $wechat->server->setMessageHandler(function($message){
             return "欢迎关注 overtrue！";
         });
 
-        // Log::info('return response.');
+        Log::info('return response.');
 
         return $wechat->server->serve();
     }

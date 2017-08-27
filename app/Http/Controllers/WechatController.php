1adfbecc422145798a6f6f3f74149416d098d81e
Corwien <corwien@126.com>
2017-08-27 23:53:09 +0800

    Add message push

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -19,8 +19,35 @@ class WechatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 overtrue！";
+        $userApi = $wechat->user;
+        $wechat->server->setMessageHandler(function($message) use($userApi){
+            switch ($message->MsgType) {
+                case 'event':
+                    return '收到事件消息';
+                    break;
+                case 'text':
+                    return '你好 '. $userApi->get($message->FromUserName)->nickname;
+                    break;
+                case 'image':
+                    return '收到图片消息';
+                    break;
+                case 'voice':
+                    return '收到语音消息';
+                    break;
+                case 'video':
+                    return '收到视频消息';
+                    break;
+                case 'location':
+                    return '收到坐标消息';
+                    break;
+                case 'link':
+                    return '收到链接消息';
+                    break;
+                // ... 其它消息
+                default:
+                    return '收到其它消息';
+                    break;
+            }
         });
 
         Log::info('return response.');

ec208e3a0079eb7c05b0ce86d0aabd6a620059e8
Corwien <corwien@126.com>
2017-08-28 22:55:12 +0800

    test

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -17,6 +17,7 @@ class WechatController extends Controller
     {
         $ret = json_encode($_REQUEST);
 
+        Log::write('info', $ret);
         Log::info("request arrived:{$ret}"); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
         $wechat = app('wechat');
